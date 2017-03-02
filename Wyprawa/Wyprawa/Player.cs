2bff608f3a8034c8da768c20e101a6f331a45d75
shmat <shmat@DESKTOP-24HCK6C>
2017-03-02 12:16:08 +0100

    Player class

---
+++ Wyprawa/Wyprawa/Player.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyprawa
+{
+    class Player : Mover
+    {
+        private Weapon equippedWeapon;
+
+        public int HitPoints { get; private set; }
+
+        private List<Weapon> inventory = new List<Weapon>();
+        public IEnumerable<string> Weapons
+        {
+            get
+            {
+                List<string> names = new List<string>();
+                foreach (Weapon weapon in inventory)
+                    names.Add(weapon.Name);
+                return names;
+            }
+        }
+
+        public Player(Game game, Point location)
+            : base(game, location)
+        {
+            HitPoints = 10;
+        }
+
+        public void Hit(int maxDamage, Random random)
+        {
+            HitPoints -= random.Next(1, maxDamage);
+        }
+
+        public void IncreaseHealth(int health, Random random)
+        {
+            HitPoints += random.Next(1, health);
+        }
+
+        public void Equip(string weaponName)
+        {
+            foreach (Weapon weapon in inventory)
+                if (weapon.Name == weaponName)
+                    equippedWeapon = weapon;
+        }
+
+        public void Move(Direction direction)
+        {
+            base.location = Move(direction, game.Boundaries);
+            if (!game.WeaponInRoom.PickedUp)
+            {
+                if (Nearby(location, 1))
+                    inventory.Add(game.WeaponInRoom);
+                if (inventory.Count == 1)
+                    this.Equip(inventory[0].Name);
+            }
+        }
+
+        public void Attack(Direction direction, Random random)
+        {
+            if (this.equippedWeapon == null)
+                return;
+            else if (equippedWeapon is IPotion)
+            {
+                equippedWeapon.Attack(direction);
+                inventory.Remove(equippedWeapon);
+            }
+            else
+                equippedWeapon.Attack(direction, random);
+        }
+    }
+}
