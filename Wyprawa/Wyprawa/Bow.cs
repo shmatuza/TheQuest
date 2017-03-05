3298c8fcc8ac5a3dcff04966eb5b61457fe47742
shmat <shmat@DESKTOP-24HCK6C>
2017-03-05 14:54:36 +0100

    Bow added

---
+++ Wyprawa/Wyprawa/Bow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyprawa
+{
+    class Bow : Weapon
+    {
+        public Bow(Game game, Point location) : base(game, location) { }
+
+        public override string Name { get { return "Łuk";} }
+
+        public override void Attack(Direction direction, Random random)
+        {
+            DamageEnemy(direction, 30, 1, random);
+        }
+    }
+}
