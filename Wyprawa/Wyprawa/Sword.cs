f18103981858e1be2b2462728dc7f0d67ffd7fe1
shmat <shmat@DESKTOP-24HCK6C>
2017-03-05 14:51:07 +0100

    clockwisedirections & sword weapon added

---
+++ Wyprawa/Wyprawa/Sword.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyprawa
+{
+    class Sword : Weapon
+    {
+        public Sword(Game game, Point location)
+            : base (game, location) { }
+
+        public override string Name { get { return "Miecz"; } }
+
+        public override void Attack(Direction direction, Random random)
+        {
+            if (!DamageEnemy(direction, 10, 3, random))
+                if (!DamageEnemy(ClockWiseDirection(direction), 10, 3, random))
+                    DamageEnemy(CounterClockWiseDirection(direction), 10, 3, random);  
+        }
+    }
+}
