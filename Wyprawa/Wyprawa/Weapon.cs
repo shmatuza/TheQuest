bdf6411aa46150047febdee70774b381ad3f13b4
shmat <shmat@DESKTOP-24HCK6C>
2017-03-05 13:50:39 +0100

    Weapon class prothotype

---
+++ Wyprawa/Wyprawa/Weapon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyprawa
+{
+    abstract class Weapon : Mover
+    {
+        public bool PickedUp { get; private set; }
+
+        public Weapon(Game game, Point location) : base(game, location)
+        {
+            PickedUp = false;
+        }
+
+        public void PickUpWeapon() { PickedUp = true; }
+
+        public abstract string Name { get; }
+
+        public abstract void Attack(Direction direction, Random random);
+
+        protected bool DamageEnemy(Direction direction, int radius, int damage, Random random)
+        {
+            Point target = game.PlayerLocation;
+            for (int distance = 0; distance < radius; distance++)
+            {
+                foreach (Enemy enemy in game.Enemies)
+                {
+                    if (Nearby(enemy.Location, target, distance))
+                    {
+                        enemy.Hit(damage, random);
+                        return true;
+                    }
+                }
+                target = Move(direction, target, game.Boundaries);
+            }
+            return false;
+        }
+
+        public Point Move(Direction direction, Point target, Rectangle boundaries)
+        {
+            location = target;
+            return Move(direction, game.Boundaries);          
+        }
+
+        public bool Nearby(Point locationToCheck, Point target, int distance)
+        {
+            location = target;
+            return Nearby(locationToCheck, distance);
+        }
+    }
+}
