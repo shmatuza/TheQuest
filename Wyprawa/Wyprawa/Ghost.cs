59229d1067db0077045aa7e73d417417ecf9dd13
shmat <shmat@DESKTOP-24HCK6C>
2017-03-05 13:12:55 +0100

    Ghost enemy

---
+++ Wyprawa/Wyprawa/Ghost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyprawa
+{
+    class Ghost : Enemy
+    {
+        public Ghost(Game game, Point location) 
+            : base(game, location, 8) { }
+
+        public override void Move(Random random)
+        {
+            if (HitPoints >= 1)
+            {
+                if (random.Next(3) == 0)
+                    location = Move(FindPlayerDirection(game.PlayerLocation), game.Boundaries);
+                if (NearPlayer())
+                    game.HitPlayer(3, random);
+            }
+        }
+
+    }
+}
