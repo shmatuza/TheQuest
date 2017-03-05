8f5bdaaa4276cfd31ad73809f5061d6a36ef54ca
shmat <shmat@DESKTOP-24HCK6C>
2017-03-05 13:18:44 +0100

    Ghoul enemy

---
+++ Wyprawa/Wyprawa/Ghoul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyprawa
+{
+    class Ghoul : Enemy
+    {
+        public Ghoul(Game game, Point location)
+            : base(game, location, 10) { }
+
+        public override void Move(Random random)
+        {
+            if (HitPoints >= 1)
+            {
+                if (random.Next(3) == 0 || random.Next(3) == 1)
+                    location = Move(FindPlayerDirection(game.PlayerLocation), game.Boundaries);
+                if (NearPlayer())
+                    game.HitPlayer(4, random);
+            }
+        }
+    }
+}
