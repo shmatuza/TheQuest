4a7840813f05ddfa2d3efe3de4f05ed4b0238224
shmat <shmat@DESKTOP-24HCK6C>
2017-03-05 15:13:12 +0100

    RedPotion added

---
+++ Wyprawa/Wyprawa/RedPotion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyprawa
+{
+    class RedPotion : Weapon, IPotion
+    {
+        public RedPotion(Game game, Point location) : base (game, location) { Used = false; }
+
+        public override string Name { get { return "Czerwona mikstura"; } }
+
+        public bool Used { get; private set; }
+
+        public override void Attack(Direction direction, Random random)
+        {
+            game.IncreasePlayerHealth(5, random);
+            Used = true;
+        }
+    }
+}
