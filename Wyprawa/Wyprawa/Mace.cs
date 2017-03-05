f763f59d678996f23a378e87efe67e1050f68fe0
shmat <shmat@DESKTOP-24HCK6C>
2017-03-05 14:59:25 +0100

    Mace added

---
+++ Wyprawa/Wyprawa/Mace.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyprawa
+{
+    class Mace : Weapon
+    {
+        public Mace(Game game, Point location) : base(game, location) { }
+
+        public override string Name { get { return "Buława"; } }
+
+        public override void Attack(Direction direction, Random random)
+        {
+            if (!DamageEnemy(Direction.Right, 20, 6, random))
+                if (!DamageEnemy(Direction.Down, 20, 6, random))
+                    if (!DamageEnemy(Direction.Left, 20, 6, random))
+                        DamageEnemy(Direction.Up, 20, 6, random);
+        }
+    }
+}
